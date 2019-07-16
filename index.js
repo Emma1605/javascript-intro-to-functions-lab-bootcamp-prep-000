68e8f3359e006ef57fdc058d1d250f0c51ce1c69
debonair-proc-5718 <jeanemma6@gmail.com>
2019-07-16 16:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,17 +6,6 @@ function whisper(string)
 {
   return string.toLowerCase()
 }
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase
-
-mixedCase.toUpperCase() === mixedCase
 function logShout(string)
 {
   console.log("HELLO")
