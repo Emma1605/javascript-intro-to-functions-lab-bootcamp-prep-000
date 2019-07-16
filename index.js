f25a9ee5f57c315175c116eabfd8fb87a75d1d02
debonair-proc-5718 <jeanemma6@gmail.com>
2019-07-16 16:23:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ function logWhisper(string)
   function sayHiToGrandma(string)
   {
     if(string.toLowerCase){return "I can't hear you!";}
-   if(string.toUpperCase){return "YES INDEED!";}
+   if(string.toUpperCase){return "yes indeed!";}
   }
