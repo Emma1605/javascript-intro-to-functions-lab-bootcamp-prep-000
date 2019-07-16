b3bb4cfd4a25ec3146506e4ba59c16918ff32a91
debonair-proc-5718 <jeanemma6@gmail.com>
2019-07-16 16:13:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function logWhisper(string)
 }
   function sayHiToGrandma(string)
   {
-    if(string===lowercase){return "I can't hear you!";}
+    if(string.toLowerCase){return "I can't hear you!";}
     else {return "YES INDEED!"}
   }
